ae5cf0ea6cb2aca13dde4b1a31f65d057a072817
pocupine18 <mehul.varma18@gmail.com>
2021-10-30 15:12:40 -0500

    alloc code done

--- MP4/MP4_Sources/MP4_Sources/vm_pool.C
+++ MP4/MP4_Sources/MP4_Sources/vm_pool.C
@@ -96,6 +96,7 @@ unsigned long VMPool::allocate(unsigned long _size) {
 
     /*_______ initialize free and alloc lists _______*/
     bool found_free_region_free = false;
+    bool found_free_region_alloc = false;
 
     unsigned int idx = 0;
     
@@ -112,13 +113,15 @@ unsigned long VMPool::allocate(unsigned long _size) {
 
             // find right index in alloc list to insert new region
             j = 0; 
-
-            while(j<512 && !(this->freelist_start_arr[j] == 0 && this->freelist_end_arr[j] == 0)){
+            while(!(this->alloclist_start_arr[j] == 0 && this->alloclist_end_arr[j] == 0) && j<512){
+                Console::puts("     -> allocate:     Alloc[");Console::puti(j);Console::puts("] = ");Console::puti((unsigned int)this->alloclist_start_arr[j]);Console::puts(" -> "); Console::puti((unsigned int)this->alloclist_end_arr[j]);Console::puts("\n");
+                found_free_region_alloc = true;
                 j++;
             }
-            j++;
             
-                Console::puts("     -> allocate: use Alloc[");Console::puti(j);Console::puts("] = ");Console::puti((unsigned int)this->alloclist_start_arr[j]);Console::puts(" -> "); Console::puti((unsigned int)this->alloclist_end_arr[j]);Console::puts("\n");
+            assert(found_free_region_alloc)
+            
+                Console::puts("     -> allocate:     Alloc[");Console::puti(j);Console::puts("] = ");Console::puti((unsigned int)this->alloclist_start_arr[j]);Console::puts(" -> "); Console::puti((unsigned int)this->alloclist_end_arr[j]);Console::puts("\n");
 
             this->alloclist_start_arr[j] = this->freelist_start_arr[idx];
             this->alloclist_end_arr[j]   = this->freelist_start_arr[idx] + (pages_to_alloc * PAGE_SIZE) - 1;
