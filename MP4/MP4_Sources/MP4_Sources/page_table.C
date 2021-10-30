3ccae27303b43e3416dbfa1b6649170dfca1e179
pocupine18 <mehul.varma18@gmail.com>
2021-10-30 16:20:16 -0500

    alloc code done

--- MP4/MP4_Sources/MP4_Sources/page_table.C
+++ MP4/MP4_Sources/MP4_Sources/page_table.C
@@ -230,7 +230,7 @@ void PageTable::free_page(unsigned long _page_no) {
       this->process_mem_pool->release_frames(frame_to_free);
    }
 
-   *pte_of_vaddr = PTE_address(vaddr);
+   pte_of_vaddr = PTE_address(vaddr);
    Console::puts("         -> free_page: new PTE = "); print_array_long(pte_of_vaddr);
 
 
