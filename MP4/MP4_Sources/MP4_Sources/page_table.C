470d7eacf29181e536f77141d2c3758b83f0cacf
pocupine18 <mehul.varma18@gmail.com>
2021-10-29 20:08:40 -0500

    alloc code done

--- MP4/MP4_Sources/MP4_Sources/page_table.C
+++ MP4/MP4_Sources/MP4_Sources/page_table.C
@@ -78,7 +78,7 @@ PageTable::PageTable()
 
    for(i = 0; i < ENTRIES_PER_PAGE; i++){
       
-      page_table_ptr_tmp[i] = (virtual_address) | 3;
+      page_table_ptr_tmp[i] = (0) | 3;
 
       virtual_address = virtual_address + 4096;
    }   
@@ -161,7 +161,7 @@ void PageTable::handle_fault(REGS* _r)
       unsigned long* new_ptp_vaddr = (unsigned long*) ((new_ptp_frame_num_phy << 12) | 0xFFC00000); // getting vaddr of ptp for iteration
 
       for(int i = 0; i < ENTRIES_PER_PAGE; i++){
-         new_ptp_vaddr[i] = 0 | 4;
+         new_ptp_vaddr[i] = (iter_vaddr) | 4;
          iter_vaddr += 4096;
       }   
 
