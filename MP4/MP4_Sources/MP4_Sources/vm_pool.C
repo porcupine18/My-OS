d948a555a3b00496a7259c014bfcbb3be7d53dcf
pocupine18 <mehul.varma18@gmail.com>
2021-10-30 08:38:10 -0500

    alloc code done

--- MP4/MP4_Sources/MP4_Sources/vm_pool.C
+++ MP4/MP4_Sources/MP4_Sources/vm_pool.C
@@ -136,7 +136,7 @@ void VMPool::release(unsigned long _start_address) {
 bool VMPool::is_legitimate(unsigned long _address) {
 
                 Console::puts("         -> is_legitimate: base_address     =");Console::puti((unsigned int)this->_base_address);Console::puts("\n");
-                Console::puts("         -> is_legitimate: first check end  =");Console::puti((unsigned int)(this->_base_address + PAGE_SIZE*2));Console::puts("\n");
+                Console::puts("         -> is_legitimate: first check end  =");Console::puti((unsigned int)(this->_base_address + PAGE_SIZE*2)-1);Console::puts("\n");
                 Console::puts("         -> is_legitimate: checking address =");Console::puti((unsigned int)_address);Console::puts("\n");
 
     // if address belongs to free/alloc lists' region
