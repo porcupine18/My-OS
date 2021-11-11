ef9ba3b091e9e878846b979d3a4350075e3d2cc8
pocupine18 <mehul.varma18@gmail.com>
2021-11-10 21:24:15 -0600

    init mp5

--- MP5/MP5_Sources/MP5_Sources/kernel.C
+++ MP5/MP5_Sources/MP5_Sources/kernel.C
@@ -305,8 +305,8 @@ int main() {
 
     /* WE ADD thread2 - thread4 TO THE READY QUEUE OF THE SCHEDULER. */
 
-    SYSTEM_SCHEDULER->add(thread2);
-    SYSTEM_SCHEDULER->add(thread3);
+    //SYSTEM_SCHEDULER->add(thread2);
+    //SYSTEM_SCHEDULER->add(thread3);
     //SYSTEM_SCHEDULER->add(thread4);
 
 #endif
