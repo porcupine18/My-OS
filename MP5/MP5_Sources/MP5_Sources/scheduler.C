6a693aad9495ed7b7f1b05e54f03cfcaa04c0827
pocupine18 <mehul.varma18@gmail.com>
2021-11-11 03:02:18 -0600

    alloc code done

--- MP5/MP5_Sources/MP5_Sources/scheduler.C
+++ MP5/MP5_Sources/MP5_Sources/scheduler.C
@@ -37,54 +37,56 @@ Scheduler::Scheduler() {
 
 void Scheduler::yield(){
 
+  
+
+  //   Thread* tmp;
+
+  //   if(curr == this->ready_head){
+  //     this->ready_head = this->ready_head->next;
+  //     Console::puts("     -> yield: popped from start\n");
+  //   }
+  //   else{
+  //     tmp = this->ready_head;
+  //     while(tmp->next){
+  //       if(tmp->next == curr){
+  //         tmp->next = curr->next;
+  //         Console::puts("     -> yield: popped \n");
+  //         break;
+  //       }
+  //       tmp = tmp->next;
+  //     }
+  //   }
+    
+  //   Console::puts("     -> yield: LL [ ");  
+  //   tmp = this->ready_head;
+  //   while(tmp){
+  //     Console::puti((unsigned int)tmp);Console::puts(" -> ");
+  //     tmp = tmp->next;
+  //   }
+  //   Console::puts(" ]\n");  
+
+
+
+  // if(curr->next == NULL){
+  //   Thread::dispatch_to(this->ready_head);
+  //       Console::puts("     -> yield: same continued - curr = ");Console::puti((int)curr);Console::puts("\n");  
+  // }
+  // else{
+  //       Console::puts("     -> yield: yield to -   new curr = ");Console::puti((int)curr);Console::puts("\n"); 
+
   Thread* curr = Thread::CurrentThread();
 
-        Console::puts("     -> yield: start -           curr = ");Console::puti((int)curr);Console::puts("\n");  
+  Console::puts("     -> yield: start -           curr = ");Console::puti((int)curr);Console::puts("\n"); 
+ 
+  Thread::dispatch_to(this->ready_head);    
 
-    Thread* tmp;
-
-    if(curr == this->ready_head){
-      this->ready_head = this->ready_head->next;
-      Console::puts("     -> yield: popped from start\n");
-    }
-    else{
-      tmp = this->ready_head;
-      while(tmp->next){
-        if(tmp->next == curr){
-          tmp->next = curr->next;
-          Console::puts("     -> yield: popped \n");
-          break;
-        }
-        tmp = tmp->next;
-      }
-    }
-    
-    Console::puts("     -> yield: LL [ ");  
-    tmp = this->ready_head;
-    while(tmp){
-      Console::puti((unsigned int)tmp);Console::puts(" -> ");
-      tmp = tmp->next;
-    }
-    Console::puts(" ]\n");  
-
-
-
-  if(curr->next == NULL){
-    Thread::dispatch_to(this->ready_head);
-        Console::puts("     -> yield: same continued - curr = ");Console::puti((int)curr);Console::puts("\n");  
-  }
-  else{
-        Console::puts("     -> yield: yield to -   new curr = ");Console::puti((int)curr);Console::puts("\n");  
-    Thread::dispatch_to(this->ready_head);    
-  }
-  
   Console::puts("+++++++++++++++++++++ Yield  Done +++++++++++++++++++++\n");
 
 }
 
 void Scheduler::resume(Thread * _thread) {
 
-        Console::puts("     -> resume: LL [ ");  
+        Console::puts("     -> resume:     LL [ ");  
         Thread* curr = this->ready_head;
         while(curr){
           Console::puti((unsigned int)curr);Console::puts(" -> ");
@@ -92,6 +94,34 @@ void Scheduler::resume(Thread * _thread) {
         }
         Console::puti((unsigned int)_thread);Console::puts(" ]\n");  
 
+
+  /*_______ remove from list _______*/
+  if(_thread == this->ready_head){
+    Console::puts("     -> resume: deleted from head \n");  
+    this->ready_head = this->ready_head->next;
+  }
+  else{
+    Thread* tmp = this->ready_head;
+    while(tmp->next){
+      if(tmp->next == _thread){
+        tmp->next = _thread->next;
+        Console::puts("     -> resume: deleted from middle \n");  
+        break;
+      }
+      tmp = tmp->next;
+    }
+  }
+
+        Console::puts("     -> resume: new LL [ ");  
+        Thread* curr = this->ready_head;
+        while(curr){
+          Console::puti((unsigned int)curr);Console::puts(" -> ");
+          curr = curr->next;
+        }
+        Console::puti((unsigned int)_thread);Console::puts(" ]\n");  
+
+
+  /*_______ push to list _______*/
   _thread->next = NULL;
 
   if(this->ready_head == NULL){
