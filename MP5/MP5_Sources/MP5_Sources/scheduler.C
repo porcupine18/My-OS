f43887d1fa444dba740d8caed8787fb312015bb4
pocupine18 <mehul.varma18@gmail.com>
2021-11-11 19:47:39 -0600

    mp5 testing funs

--- MP5/MP5_Sources/MP5_Sources/scheduler.C
+++ MP5/MP5_Sources/MP5_Sources/scheduler.C
@@ -33,34 +33,74 @@ Scheduler::Scheduler() {
 
 void Scheduler::yield() {
 
+    Console::puts("     -> yield: start");
     Thread* next = this->ready_head;
 
     this->ready_head = this->ready_head->next;
+
+    Thread::CurrentThread()->next = NULL;
+
+                      // print -------------------------------------------------------
+                          Console::puts("     -> yield:     LL [ ");  
+                          Thread* curr = this->ready_head;
+                          while(curr){
+                            Console::puti((unsigned int)curr);Console::puts(" -> ");
+                            curr = curr->next;
+                          }
+                      // print -------------------------------------------------------
+
 
     Thread::dispatch_to(next);
 }
 
 void Scheduler::resume(Thread * _thread) {
-        
+
+                      // print -------------------------------------------------------
+                          Console::puts("     -> resume:     LL [ ");  
+                          Thread* curr = this->ready_head;
+                          while(curr){
+                            Console::puti((unsigned int)curr);Console::puts(" -> ");
+                            curr = curr->next;
+                          }
+                      // print -------------------------------------------------------
+  
+    Console::puts("     -> resume: start");
+
     if(this->ready_head == NULL){
+        Console::puts("     -> resume: add to start");
         this->ready_head = _thread;
         this->ready_tail = _thread;
     }
     else{
+        Console::puts("     -> resume: add to end");
         this->ready_tail->next = _thread;
         this->ready_tail = _thread;
     }
+
 }
 
 void Scheduler::add(Thread * _thread) {
     
+                      // print -------------------------------------------------------
+                          Console::puts("     -> add:     LL [ ");  
+                          Thread* curr = this->ready_head;
+                          while(curr){
+                            Console::puti((unsigned int)curr);Console::puts(" -> ");
+                            curr = curr->next;
+                          }
+                      // print -------------------------------------------------------
+
+    Console::puts("     -> add: start");
+
     _thread->next = NULL;
     
     if(this->ready_head == NULL){
+        Console::puts("     -> add: add to start");
         this->ready_head = _thread;
         this->ready_tail = _thread;
     }
     else{
+        Console::puts("     -> add: add to end");
         this->ready_tail->next = _thread;
         this->ready_tail = _thread;
     }
