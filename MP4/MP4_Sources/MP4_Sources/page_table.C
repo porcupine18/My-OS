dfe7b5a558012df15dc6c0e6421071d59d6264fc
pocupine18 <mehul.varma18@gmail.com>
2021-11-12 15:26:24 -0600

    mp4 regrade test

--- MP4/MP4_Sources/MP4_Sources/page_table.C
+++ MP4/MP4_Sources/MP4_Sources/page_table.C
@@ -228,14 +228,11 @@ void PageTable::free_page(unsigned long _page_no) {
    if(*pte_of_vaddr & 1){
          Console::puts("            -> free_page: Freeing valid frame no= ");Console::puti(frame_to_free);Console::puts("\n");
       this->process_mem_pool->release_frames(frame_to_free);
-         Console::puts("            -> free_page: Release frame done");Console::puts("\n");
+      write_cr3(read_cr3());
    }
 
    *pte_of_vaddr = *pte_of_vaddr >> 2;
    *pte_of_vaddr = *pte_of_vaddr << 2;   
-
-   //write_cr3(read_cr3());
-
          Console::puts("            -> free_page: new PTE ="); print_array_long(pte_of_vaddr);
 
    Console::puts("            -> free_page: DONE!\n");
