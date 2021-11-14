6d2af2a5c5a67e34c6b11c88d01b7089f9f1098b
pocupine18 <mehul.varma18@gmail.com>
2021-11-13 23:31:13 -0600

    mp5 testing roundrobin

--- MP5/MP5_Sources/MP5_Sources/eoq_timer.C
+++ MP5/MP5_Sources/MP5_Sources/eoq_timer.C
@@ -45,14 +45,12 @@ void EOQTimer::handle_interrupt(REGS *_r) {
         seconds++;
         ticks = 0;
     }
-
     
     Console::puts("\n\n!!!!! Timer interrupt --> Preempting !!!!! \n\n");
 
     SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
     SYSTEM_SCHEDULER->yield();
     Machine::outportb(0x20, 0x20);
-
 }
 
 
--- MP5/MP5_Sources/MP5_Sources/scheduler.C
+++ MP5/MP5_Sources/MP5_Sources/scheduler.C
@@ -43,7 +43,7 @@ Scheduler::Scheduler() {
 	this->zombie_head = NULL;
 	this->zombie_tail = NULL;
 
-    EOQTimer timer(20); /* timer ticks every 10ms. */
+    EOQTimer timer = new EOQTimer(20); /* timer ticks every 10ms. */
     InterruptHandler::register_handler(0, &timer);
 
 	Console::puts("\n\n++++++++++++++++ Constructed Scheduler ++++++++++++++++\n\n");
