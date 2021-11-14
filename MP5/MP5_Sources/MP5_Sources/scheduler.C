f5e89b22ee84ca3356acb3259bac6782501e7b87
pocupine18 <mehul.varma18@gmail.com>
2021-11-13 23:31:39 -0600

    mp5 testing roundrobin

--- MP5/MP5_Sources/MP5_Sources/scheduler.C
+++ MP5/MP5_Sources/MP5_Sources/scheduler.C
@@ -43,8 +43,8 @@ Scheduler::Scheduler() {
 	this->zombie_head = NULL;
 	this->zombie_tail = NULL;
 
-    EOQTimer timer = new EOQTimer(20); /* timer ticks every 10ms. */
-    InterruptHandler::register_handler(0, &timer);
+    EOQTimer* timer = new EOQTimer(20); /* timer ticks every 10ms. */
+    InterruptHandler::register_handler(0, timer);
 
 	Console::puts("\n\n++++++++++++++++ Constructed Scheduler ++++++++++++++++\n\n");
 }
