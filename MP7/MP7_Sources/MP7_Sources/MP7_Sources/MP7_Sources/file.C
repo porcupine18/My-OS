967dd0f2b1db802eb01e05cacdeaf58189df6ced
pocupine18 <mehul.varma18@gmail.com>
2021-12-12 21:11:42 -0600

    mp7 init

--- MP7/MP7_Sources/MP7_Sources/MP7_Sources/MP7_Sources/file.C
+++ MP7/MP7_Sources/MP7_Sources/MP7_Sources/MP7_Sources/file.C
@@ -134,7 +134,10 @@ int File::Write(unsigned int _n, const char *_buf) {
     Console::puts(";  told to write=");      Console::puti(_n);Console::puts("\n");
 
     // write to file , update size in inode, update seek position
-    memcpy(this->block_cache+this->seek_position, _buf, to_write);
+    for(int ct=0; ct<to_write; ct++){
+        this->block_cache[this->seek_position+ct] = _buf[ct];
+    }
+    //memcpy(this->block_cache+this->seek_position, _buf, to_write);
 
     this->seek_position += to_write;
     this->file_inode->file_size += to_write;
