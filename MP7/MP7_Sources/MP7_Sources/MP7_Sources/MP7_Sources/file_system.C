47484400131f9fb30c9b709e21eb993013313350
pocupine18 <mehul.varma18@gmail.com>
2021-12-12 18:53:47 -0600

    mp7 init

--- MP7/MP7_Sources/MP7_Sources/MP7_Sources/MP7_Sources/file_system.C
+++ MP7/MP7_Sources/MP7_Sources/MP7_Sources/MP7_Sources/file_system.C
@@ -82,13 +82,14 @@ bool FileSystem::Mount(SimpleDisk * _disk) {
         Console::puts("\n");
     }
 
-    Console::puts("     -> Mount: free_list-");
+    Console::puts("     -> Mount: free_list-\n");
     for(int i=0; i<MAX_MAPPED_BLOCKS; i++){
         Console::puts("[");     Console::puti(i); 
         Console::puts("]=");                            Console::puti((int)free_list[i]);
         if(i%10 == 0)
             Console::puts("\n");
     }
+    Console::puts("\n");
 
     Console::puts("++++++++++ Mounting DONE ++++++++++\n\n");
     return true;
@@ -133,13 +134,14 @@ bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size, FileSystem* _fs)
     free_buf[0] = 1; // 1 = BUSY, 0 = FREE
     free_buf[1] = 1; // 1 = BUSY, 0 = FREE
 
-    Console::puts("     -> Format: free_buf-");
+    Console::puts("     -> Format: free_buf-\n");
     for(int i=0; i<MAX_MAPPED_BLOCKS; i++){
         Console::puts("[");     Console::puti(i); 
         Console::puts("]=");                            Console::puti((int)free_buf[i]);
         if(i%10 == 0)
             Console::puts("\n");
     }
+    Console::puts("\n");
 
 
     // write empty inode list and free list to the disk
